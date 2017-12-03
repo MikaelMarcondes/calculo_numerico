87ed02fa4496fbec9d0191a2c1749266e60321d9
Mikael Marcondes <mikael.oliveira@usp.br>
2017-12-03 17:00:06 -0200

    Update pvi_edo.c

--- pvi_edo.c
+++ pvi_edo.c
@@ -14,29 +14,47 @@
 #define FALSE	0
 #define TRUE	1
 
+/*Variaveis globais*/
 
-/*O valor que aceitaremos como verdadeiro é erf(0.9083010)=0.80104398333355385655425471455314706359364542054.*/
-
-/*Variáveis globais*/
-
+double C_S=332 //velocidade do som, em m/s
+double A=M_PI*(pow(0.15, 2)/4); //área da seção transversa, em m2
+double rho=1.2 //densidade em kg/m3
 double M[9]={0, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3};
 double C_D[9]={1.6, 1.8, 2.1, 4.8, 5.8, 5.4, 4.8, 4.2, 3.9};
-
-/*Vetores de pesos e abcissas a serem usados na quadratura por Gauss-Legendre*/
-
-
-
-/*Vetores de pesos e abcissas a serem usados na quadratura por Gauss-Kronrod*/
-
-
 
 /*Protótipos de funções usadas ao longo do programa*/
 
 /*Programa principal*/
 
 int main(){
-	int i=0;
-	float j;
 
-	return 0;
+}
+
+double lagr_pol(double a, double b, double c, double A, double B, double C, double x){
+    return ((A*(x-b)*(x-c)/((a-b)*(a-c)))+(B*(x-a)*(x-c)/((b-a)*(b-c)))+(C*(x-a)*(x-b)/((c-a)*(c-b))));
+}
+
+double drag_force(double v[2]){
+    double s;
+    double d;
+    s=speed(v);
+    d=drag_coefficient(s);
+    return (A*d*rho*pow(s,2)/2);
+}
+
+double drag_coefficient(double s){
+    int i,j,k;
+    i=j=k=0;
+    s=s/C_s;    //numero de Mach
+    for(i=0; i<9; i=i+3){
+        j=i+1;
+        k=i+2
+        if(M[i]<=s && s<=M[k]){
+            return lagr_pol(M[i], M[j], M[k], C_D[i], C_D[j], C_D[k], s);
+        }
+    }
+}
+
+double speed(double v[2]){
+    return (pow(pow(v[0], 2)+pow(v[1], 2)), 0.5);
 }
