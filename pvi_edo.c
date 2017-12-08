6e6b3f6ed9bd20052b317ecee9ed5f601e23917b
Mikael Marcondes <mikael.oliveira@usp.br>
2017-12-08 12:10:13 -0200

    Update pvi_edo.c

--- pvi_edo.c
+++ pvi_edo.c
@@ -16,47 +16,59 @@
 
 /*Variaveis globais*/
 
-double C_S=332 //velocidade do som, em m/s
+double C_S=332; //velocidade do som, em m/s
 double A=M_PI*(pow(0.15, 2)/4); //área da seção transversa, em m2
-double rho=1.2 //densidade em kg/m3
+double rho=1.2; //densidade em kg/m3
 double M[9]={0, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3};
 double C_D[9]={1.6, 1.8, 2.1, 4.8, 5.8, 5.4, 4.8, 4.2, 3.9};
-double g=9.79 //gravidade em m/s2
-double V0=700 //velocidade inicial em m/s
-double m=50 //massa em kg
+double g=9.79; //gravidade em m/s2
+double V0=700; //velocidade inicial em m/s
+double m=50; //massa em kg
 
 /*Protótipos de funções usadas ao longo do programa*/
 double lagr_pol(double a, double b, double c, double A, double B, double C, double x);
-double drag_force(double v[2]);
 double drag_coefficient(double s);
 double speed(double v[2]);
-double runge_kutta_fehlberg(double k[6], double x_n, double y_n, double h);
+void runge_kutta_fehlberg(double v[2], int flaf, double dt);
+double f(double v_1, int flag, double v_2);
 
 /*Programa principal*/
 
 int main(){
+    double x[2]={0,0};
 
+    double a;
+    printf("Entre com o ângulo inicial (inteiro de 1 a 6):");
+    scanf("%lf",&a);
+    a *= 10*M_PI*N2/180;
+    double v[2]={V0*cos(a), V0*sin(a)};
+    double z[2]={0,0}; //vetor para guardar temporariamente as velocidades antigas
+
+    int i;
+    double dt=1E-3;
+
+    for(i=0; x[1]>=0; i++){
+        z[0]=v[0];
+        z[1]=v[1];
+        runge_kutta_fehlberg(v, TRUE, dt); //com flag=TRUE altera a primeira componente da velocidade
+        runge_kutta_fehlberg(v, FALSE, dt); //com flag=FALSE altera a segunda componente da velocidade
+        x[0] += dt*v[0];
+        x[1] += dt*v[1];
+    }
 }
 
 double lagr_pol(double a, double b, double c, double A, double B, double C, double x){
     return ((A*(x-b)*(x-c)/((a-b)*(a-c)))+(B*(x-a)*(x-c)/((b-a)*(b-c)))+(C*(x-a)*(x-b)/((c-a)*(c-b))));
 }
 
-double drag_force(double v[2]){
-    double s;
-    double d;
-    s=speed(v);
-    d=drag_coefficient(s);
-    return (A*d*rho*pow(s,2)/2);
-}
 
 double drag_coefficient(double s){
     int i,j,k;
     i=j=k=0;
-    s=s/C_s;    //numero de Mach
+    s=s/C_S;    //numero de Mach
     for(i=0; i<9; i=i+3){
         j=i+1;
-        k=i+2
+        k=i+2;
         if(M[i]<=s && s<=M[k]){
             return lagr_pol(M[i], M[j], M[k], C_D[i], C_D[j], C_D[k], s);
         }
@@ -64,17 +76,29 @@ double drag_coefficient(double s){
 }
 
 double speed(double v[2]){
-    return (pow(pow(v[0], 2)+pow(v[1], 2)), 0.5);
+    return (pow((pow(v[0], 2)+pow(v[1], 2)), 0.5));
 }
 
-double runge_kutta_fehlberg(double k[6], double t_n, double y_n, double h){
-    double k[1], k[2], k[3], k[4], k[5], k[6];
-    k[1]=h*f(t_n, y_n);
-    k[2]=h*f(t_n+(h/4), y_n+(k[1]/4));
-    k[3]=h*f(t_n+(3*h/8), y_n+(3*k[1]/32)+(9*k[2]/32));
-    k[4]=h*f(t_n+(12*h/13), y_n+(1932*k[1]/2197)-(7200*k[2]/2197)+(7296*k[3]/2197));
-    k[5]=h*f(t_n+h,y_n+(439*k[1]/216)-8*k[2]+(3680*k[3]/513)-(845*k[4]/4104));
-    k[6]=h*f(t_n+(h/2),y_n-(8*k[1]/27)+2*k[2]-(3544*k[3]/2565)-(1859*k[4]/4104)-(11*k[5]/40));
-    return (y_n+(16*k[1]/135)+(6656*k[3]/12825)+(28561*k[4]/56430)-(9*k[5]/50)+(2*k[6]/55));
+void runge_kutta_fehlberg(double v[2], int flag, double dt){
+    int i=0;
+    if(flag==TRUE) i=0;
+    else i=1;
 
+    double k[6];
+
+    k[1]=dt*f(v[i], flag, v[((i+1)%2)]);
+    k[2]=dt*f(v[i]+(k[1]/4), flag, v[((i+1)%2)]);
+    k[3]=dt*f(v[i]+(3*k[1]/32)+(9*k[2]/32), flag, v[((i+1)%2)]);
+    k[4]=dt*f(v[i]+(1932*k[1]/2197)-(7200*k[2]/2197)+(7296*k[3]/2197), flag, v[((i+1)%2)]);
+    k[5]=dt*f(v[i]+(439*k[1]/216)-8*k[2]+(3680*k[3]/513)-(845*k[4]/4104), flag, v[((i+1)%2)]);
+    k[6]=dt*f(v[i]-(8*k[1]/27)+2*k[2]-(3544*k[3]/2565)-(1859*k[4]/4104)-(11*k[5]/40), flag, v[((i+1)%2)]);
+    v[i] += (16*k[1]/135)+(6656*k[3]/12825)+(28561*k[4]/56430)-(9*k[5]/50)+(2*k[6]/55);
+}
+
+double f(double v_1, int flag, double v_2){
+    double s;
+    double V[2]={v_1, v_2};
+    s=speed(V);
+    if(flag==TRUE) return (-A*rho*drag_coefficient(s)*v_1*s/(2*m));
+    else return (-A*rho*drag_coefficient(s)*v_1*s/(2*m)-m*g);
 }
